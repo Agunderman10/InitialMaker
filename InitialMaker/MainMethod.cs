149211e9fef5b37ca31c70c7859295d45b9362bf
Andrew Gunderman <agunderman10@gmail.com>
2018-11-28 17:20:27 -0500

    Initial Commit

---
+++ InitialMaker/MainMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialMaker
+{
+    class MainMethod
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is desired first name?");
+            string userFirstName = Console.ReadLine();
+
+            Console.WriteLine("What is desired middle name?");
+            string userMiddleName = Console.ReadLine();
+
+            Console.WriteLine("What is desired last name?");
+            string userLastName = Console.ReadLine();
+
+            //instantialize person's name class
+            Name personName = new Name(userFirstName, userMiddleName, userLastName);
+            string fullName, inits;
+            fullName = personName.ToString();
+            inits = personName.Initials();
+            Console.WriteLine("The name is {0}.", fullName);
+            Console.WriteLine("The initials are {0}.", inits);
+
+            //keep console open
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ InitialMaker/Name.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace InitialMaker
+{
+    public struct Name
+    {
+        //private variables
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+
+        //constructor sets first, middle, and last to private variables
+        public Name(string first, string middle, string last)
+        {
+            this._firstName = first;
+            this._middleName = middle;
+            this._lastName = last;
+        }
+
+        //first name property
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                FirstName = _firstName;
+            }
+        }
+
+        //middle name property
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                MiddleName = _middleName;
+            }
+        }
+
+        //last name property
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                LastName = _lastName;
+            }
+        }
+
+        //override ToString() to determine format that we want to display the name and initials
+        public override string ToString()
+        {
+            return (String.Format("{0} {1} {2}", _firstName, _middleName, _lastName));
+        }
+
+        //create user initials from first letter of first middle and last name
+        public string Initials()
+        {
+            return (String.Format("{0}{1}{2}", _firstName.Substring(0, 1),
+                _middleName.Substring(0, 1), _lastName.Substring(0, 1)));
+        }
+    }
+}
